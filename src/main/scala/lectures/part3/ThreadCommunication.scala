9ed01e194a766158124a929046ab48c65a005661
Kacper Kruger <kacper.kruger1@gmail.com>
2022-09-17 17:40:42 +0200

    deadlock, livelock, notifyAll

--- src/main/scala/lectures/part3/ThreadCommunication.scala
+++ src/main/scala/lectures/part3/ThreadCommunication.scala
@@ -199,5 +199,72 @@ object ThreadCommunication extends App {
     producer2.start()
   }
 
-  multiProdConsBuffer()
+//  multiProdConsBuffer()
+
+  def testNotifyAll(): Unit = {
+    val bell = new Object
+
+    (1 to 10).foreach(i =>
+      new Thread(() => {
+        bell.synchronized {
+          println(s"[thread $i] waiting")
+          bell.wait()
+          println(s"[thread $i] hooray!")
+        }
+      }).start()
+    )
+
+    new Thread(() => {
+      Thread.sleep(2000)
+      bell.synchronized {
+        println("[announcer] start")
+        bell.notifyAll()
+      }
+    }).start()
+  }
+
+//  testNotifyAll()
+
+  case class Friend(name: String) {
+    def bow(other: Friend): Unit = {
+      this.synchronized {
+        println(s"[$this.name] i'm bowing to my friend $other")
+        other.rise(this)
+        println(s"[$this.name] my friend $other has risen")
+      }
+    }
+
+    def rise(other: Friend): Unit = {
+      this.synchronized {
+        println(s"[$this.name] i am rising to my friend $other")
+      }
+    }
+
+    var side = "right"
+    def switchSide(): Unit = {
+      if (side == "right") side = "left"
+      else side = "right"
+    }
+
+    def pass(other: Friend): Unit = {
+      while (this.side == other.side) {
+        println(s"[$this.name] Oh, $other, feel free to pass")
+        switchSide()
+        Thread.sleep(1000)
+      }
+    }
+  }
+
+  val sam = Friend("Sam")
+  val pierre = Friend("Pierre")
+
+  // deadlock
+
+//  new Thread(() => sam.bow(pierre)).start() // sam's lock, then pierre's lock
+//  new Thread(() => pierre.bow(sam)).start() // pierre's lock, then sam's lock
+
+  // livelock
+
+  new Thread(() => sam.pass(pierre)).start()
+  new Thread(() => pierre.pass(sam)).start()
 }
